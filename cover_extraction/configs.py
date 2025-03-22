1a0ac3e34fa6fdaa924c2e469aefbe1c0652d9fc
williamxieigs <yi.xie@igsl-group.com>
2025-03-22 17:24:32 +0800

    updat

--- cover_extraction/configs.py
+++ cover_extraction/configs.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-dashscope_api_key = ''
+dashscope_api_key = 'sk-88f509096c9f45c79f16cad6adbba753'
 tgi_host = "http://localhost:8080"
-huggingface_api_key = ''
+huggingface_api_key = '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'
 host = "0.0.0.0"
 port = 8081
 
 neo4j_host = "bolt://localhost:7687"
 neo4j_user = "neo4j"
 neo4j_password = "12345678"
-neo4j_db = "neo4j"
+neo4j_db = "cover"
