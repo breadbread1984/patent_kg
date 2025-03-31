4ace411f5f9a6293c693c9723f17bffb4d86eb07
williamxieigs <yi.xie@igsl-group.com>
2025-03-31 11:56:12 +0800

    update

--- knowledge_graph_generation/load_graphdb.py
+++ knowledge_graph_generation/load_graphdb.py
@@ -9,8 +9,8 @@ from transformers import AutoTokenizer
 from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredHTMLLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_neo4j import Neo4jGraph
-from langchain_experimental.graph_transformers.llm import LLMGraphTransformer
-from configs import neo4j_host, neo4j_user, neo4j_password, neo4j_db
+from langchain_experimental.graph_transformers import LLMGraphTransformer, DiffbotGraphTransformer, RelikGraphTransformer, GlinerGraphTransformer
+from configs import *
 from models import Tongyi
 
 FLAGS = flags.FLAGS
@@ -18,12 +18,20 @@ FLAGS = flags.FLAGS
 def add_options():
   flags.DEFINE_string('input_dir', default = None, help = 'path to directory')
   flags.DEFINE_boolean('split', default = False, help = 'whether to split document')
+  flags.DEFINE_enum('model', default = 'llm', enum_values = {'llm', 'diffbot', 'relik', 'gliner'}, help = 'model type')
 
 def main(unused_argv):
   llm = Tongyi()
-  graph_transformer = LLMGraphTransformer(
-    llm = llm,
-  )
+  if FLAGS.model == 'llm':
+    graph_transformer = LLMGraphTransformer(llm = llm,)
+  elif FLAGS.model == 'diffbot':
+    graph_transformer = DiffbotGraphTransformer(diffbot_api_key = diffbot_api_key)
+  elif FLAGS.model == 'relik':
+    graph_transformer = RelikGraphTransformer()
+  elif FLAGS.model == 'gliner':
+    graph_transformer = GlinerGraphTransformer()
+  else:
+    raise Exception('unknown graph transformer type!')
   neo4j = Neo4jGraph(url = neo4j_host, username = neo4j_user, password = neo4j_password, database = neo4j_db)
   if FLAGS.split:
     text_splitter = RecursiveCharacterTextSplitter(separators = [r"\n\n", r"\n", r"\.(?![0-9])|(?<![0-9])\.", r"。"], is_separator_regex = True, chunk_size = 150, chunk_overlap = 10)
