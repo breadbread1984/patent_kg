6003617b321851bcf2e630898d8871c16da32a60
yi.xie <yi.xie@igsl-group.com>
2025-04-12 07:22:05 +0000

    update

--- knowledge_graph_generation/models.py
+++ knowledge_graph_generation/models.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+from os import environ
 from langchain_openai import ChatOpenAI
+from langchain_huggingface import ChatHuggingFace
+from langchain_community.llms import HuggingFaceTextGenInference
 
 class Tongyi(ChatOpenAI):
   def __init__(self,):
@@ -12,3 +15,16 @@ class Tongyi(ChatOpenAI):
       temperature = 0.8,
     )
 
+class Qwen2_5(ChatHuggingFace):
+  def __init__(self,):
+    environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
+    super(Qwen2_5, self).__init__(
+      llm = HuggingFaceTextGenInference(
+        inference_server_url = configs.tgi_host,
+        do_sample = True,
+        top_p = 0.8,
+        temperature = 0.8,
+      ),
+      model_id = 'Qwen/Qwen2.5-7B-Instruct',
+      verbose = True
+    )
